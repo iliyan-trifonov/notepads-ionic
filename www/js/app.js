72038aaaff2da1001e239b9000de9551f2eddde0
Iliyan Trifonov <iliyan.trifonov@gmail.com>
2015-05-01 01:42:09 +0300

    added filter hrefToJS (thanks http://forum.ionicframework.com/t/how-to-opening-links-in-content-in-system-browser-instead-of-cordova-browser-wrapper/2427/17) which makes the links open in new window of the system browser

--- www/js/app.js
+++ www/js/app.js
@@ -147,4 +147,14 @@ angular.module('Notepads', [
       StatusBar.styleDefault();
     }
   });
-});
+})
+
+.filter('hrefToJS', function ($sce, $sanitize) {
+    return function (text) {
+        var regex = /href="([\S]+)"/g;
+        var newString = $sanitize(text).replace(regex, "href=\"#\" onClick=\"window.open('$1', '_blank', 'location=yes')\"");
+        return $sce.trustAsHtml(newString);
+    };
+})
+
+;
